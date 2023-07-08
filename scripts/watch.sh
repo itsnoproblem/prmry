d6a6cc455bea72ade27eed012dc221b07de073e2
itsnoproblem <noproblem@mulligan.xyz>
2023-07-08 16:14:06 -0400

    doc updates, install reflex if needed

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -7,6 +7,14 @@ while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+
+if ! hash reflex
+then
+  go install github.com/cespare/reflex@latest
+fi
+
+
 echo $DIR
 cd $DIR/../ || (echo "failed to change directory to $DIR" && exit 1)
 reflex -d fancy -s -r '\.templ' $DIR/server.sh
