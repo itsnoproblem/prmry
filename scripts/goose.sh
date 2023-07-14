b03307e92db5830d875a5be39100d91bc1b0c00f
martymulligan <2565437+martymulligan@users.noreply.github.com>
2023-07-14 01:18:33 -0400

    align docs and local setup; start tagging releases (#8)
    * align docs and local setup; start tagging releases
    
    * set go version in dockerfile
    
    * set go version in github actions

--- scripts/goose.sh
+++ scripts/goose.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-goose -s -dir=./migrations mysql "root:root@tcp(127.0.0.1)/rgb?parseTime=true&multiStatements=true" "$@"
+source "${BASH_SOURCE%/*}/functions.sh"
+DIR=$(scriptDir)
+
+if ! hash goose 2> /dev/null
+then
+  echo "Installing goose..."
+  go install github.com/pressly/goose/v3/cmd/goose@latest
+fi
+
+ENVFILE=$(realpath "$DIR/../.env")
+if [ -f "$ENVFILE" ]
+then
+  . $ENVFILE
+fi
+
+goose -s -dir=./migrations mysql "$DB_USER:$DB_PASS@tcp($DB_HOST)/$DB_NAME?parseTime=true&multiStatements=true" "$@"
--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
-SOURCE=${BASH_SOURCE[0]}
-while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
-  SOURCE=$(readlink "$SOURCE")
-  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+source "${BASH_SOURCE%/*}/functions.sh"
+DIR=$(scriptDir)
 
-
-if ! hash reflex
+if ! hash reflex 2> /dev/null
 then
+  echo "Installing reflex..."
   go install github.com/cespare/reflex@latest
 fi
 
-
-echo $DIR
-cd $DIR/../ || (echo "failed to change directory to $DIR" && exit 1)
-reflex -d fancy -s -r '\.templ' $DIR/server.sh
+cd "$DIR"/../ || (echo "failed to change directory to $DIR" && exit 1)
+reflex -d fancy -s -r '\.templ' "$DIR"/server.sh
 cd -
